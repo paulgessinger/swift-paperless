85447555510df4dae08ae699a4babcfad5aa66fd
Paul Gessinger <hello@paulgessinger.com>
2023-02-13 21:30:43 +0100

    Initial Commit

---
+++ swift-paperless/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  swift-paperless
+//
+//  Created by Paul Gessinger on 13.02.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift-paperless/swift_paperlessApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_paperlessApp.swift
+//  swift-paperless
+//
+//  Created by Paul Gessinger on 13.02.23.
+//
+
+import SwiftUI
+
+@main
+struct swift_paperlessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
