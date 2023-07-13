f8f55581ad4e4f36c05f5e337c0abc758e24b042
Paul Gessinger <hello@paulgessinger.com>
2023-07-13 16:01:59 +0300

    change format of filter rule type

--- swift-paperless/FilterRuleType.swift
+++ swift-paperless/FilterRuleType.swift
@@ -46,146 +46,146 @@ enum FilterRuleType: Int, Equatable, CaseIterable {
 
     func filterVar() -> String {
         switch self {
-            case .title:
-                return "title__icontains"
-            case .content:
-                return "content__icontains"
-            case .asn:
-                return "archive_serial_number"
-            case .correspondent:
-                return "correspondent__id"
-            case .storagePath:
-                return "storage_path__id"
-            case .documentType:
-                return "document_type__id"
-            case .isInInbox:
-                return "is_in_inbox"
-            case .hasTagsAll:
-                return "tags__id__all"
-            case .hasTagsAny:
-                return "tags__id__in"
-            case .doesNotHaveTag:
-                return "tags__id__none"
-            case .hasAnyTag:
-                return "is_tagged"
-            case .createdBefore:
-                return "created__date__lt"
-            case .createdAfter:
-                return "created__date__gt"
-            case .createdYear:
-                return "created__year"
-            case .createdMonth:
-                return "created__month"
-            case .createdDay:
-                return "created__day"
-            case .addedBefore:
-                return "added__date__lt"
-            case .addedAfter:
-                return "added__date__gt"
-            case .modifiedBefore:
-                return "modified__date__lt"
-            case .modifiedAfter:
-                return "modified__date__gt"
-            case .asnIsnull:
-                return "archive_serial_number__isnull"
-            case .asnGt:
-                return "archive_serial_number__gt"
-            case .asnLt:
-                return "archive_serial_number__lt"
-            case .titleContent:
-                return "title_content"
-            case .fulltextQuery:
-                return "query"
-            case .fulltextMorelike:
-                return "more_like_id"
+        case .title:
+            return "title__icontains"
+        case .content:
+            return "content__icontains"
+        case .asn:
+            return "archive_serial_number"
+        case .correspondent:
+            return "correspondent__id"
+        case .storagePath:
+            return "storage_path__id"
+        case .documentType:
+            return "document_type__id"
+        case .isInInbox:
+            return "is_in_inbox"
+        case .hasTagsAll:
+            return "tags__id__all"
+        case .hasTagsAny:
+            return "tags__id__in"
+        case .doesNotHaveTag:
+            return "tags__id__none"
+        case .hasAnyTag:
+            return "is_tagged"
+        case .createdBefore:
+            return "created__date__lt"
+        case .createdAfter:
+            return "created__date__gt"
+        case .createdYear:
+            return "created__year"
+        case .createdMonth:
+            return "created__month"
+        case .createdDay:
+            return "created__day"
+        case .addedBefore:
+            return "added__date__lt"
+        case .addedAfter:
+            return "added__date__gt"
+        case .modifiedBefore:
+            return "modified__date__lt"
+        case .modifiedAfter:
+            return "modified__date__gt"
+        case .asnIsnull:
+            return "archive_serial_number__isnull"
+        case .asnGt:
+            return "archive_serial_number__gt"
+        case .asnLt:
+            return "archive_serial_number__lt"
+        case .titleContent:
+            return "title_content"
+        case .fulltextQuery:
+            return "query"
+        case .fulltextMorelike:
+            return "more_like_id"
         }
     }
 
     func dataType() -> DataType {
         switch self {
-            case .title:
-                return .string
-            case .content:
-                return .string
-            case .asn:
-                return .number
-            case .correspondent:
-                return .correspondent
-            case .storagePath:
-                return .storagePath
-            case .documentType:
-                return .documentType
-            case .isInInbox:
-                return .boolean
-            case .hasTagsAll:
-                return .tag
-            case .hasTagsAny:
-                return .tag
-            case .doesNotHaveTag:
-                return .tag
-            case .hasAnyTag:
-                return .boolean
-            case .createdBefore:
-                return .date
-            case .createdAfter:
-                return .date
-            case .createdYear:
-                return .number
-            case .createdMonth:
-                return .number
-            case .createdDay:
-                return .number
-            case .addedBefore:
-                return .date
-            case .addedAfter:
-                return .date
-            case .modifiedBefore:
-                return .date
-            case .modifiedAfter:
-                return .date
-            case .asnIsnull:
-                return .boolean
-            case .asnGt:
-                return .number
-            case .asnLt:
-                return .number
-            case .titleContent:
-                return .string
-            case .fulltextQuery:
-                return .string
-            case .fulltextMorelike:
-                return .number
+        case .title:
+            return .string
+        case .content:
+            return .string
+        case .asn:
+            return .number
+        case .correspondent:
+            return .correspondent
+        case .storagePath:
+            return .storagePath
+        case .documentType:
+            return .documentType
+        case .isInInbox:
+            return .boolean
+        case .hasTagsAll:
+            return .tag
+        case .hasTagsAny:
+            return .tag
+        case .doesNotHaveTag:
+            return .tag
+        case .hasAnyTag:
+            return .boolean
+        case .createdBefore:
+            return .date
+        case .createdAfter:
+            return .date
+        case .createdYear:
+            return .number
+        case .createdMonth:
+            return .number
+        case .createdDay:
+            return .number
+        case .addedBefore:
+            return .date
+        case .addedAfter:
+            return .date
+        case .modifiedBefore:
+            return .date
+        case .modifiedAfter:
+            return .date
+        case .asnIsnull:
+            return .boolean
+        case .asnGt:
+            return .number
+        case .asnLt:
+            return .number
+        case .titleContent:
+            return .string
+        case .fulltextQuery:
+            return .string
+        case .fulltextMorelike:
+            return .number
         }
     }
 
     func defaultValue() -> Bool {
         switch self {
-            case .isInInbox:
-                return true
-            case .hasAnyTag:
-                return true
+        case .isInInbox:
+            return true
+        case .hasAnyTag:
+            return true
 
-            default:
-                return false
+        default:
+            return false
         }
     }
 
     func multiple() -> Bool {
         switch self {
-            case .hasTagsAll:
-                return true
-            case .hasTagsAny:
-                return true
-            case .doesNotHaveTag:
-                return true
+        case .hasTagsAll:
+            return true
+        case .hasTagsAny:
+            return true
+        case .doesNotHaveTag:
+            return true
 
-            default:
-                return false
+        default:
+            return false
         }
     }
 
     static func allMultiples() -> Set<FilterRuleType> {
-        return [
+        [
             .hasTagsAll,
             .hasTagsAny,
             .doesNotHaveTag,
@@ -194,15 +194,15 @@ enum FilterRuleType: Int, Equatable, CaseIterable {
 
     func isNullFilterVar() -> String? {
         switch self {
-            case .correspondent:
-                return "correspondent__isnull"
-            case .storagePath:
-                return "storage_path__isnull"
-            case .documentType:
-                return "document_type__isnull"
+        case .correspondent:
+            return "correspondent__isnull"
+        case .storagePath:
+            return "storage_path__isnull"
+        case .documentType:
+            return "document_type__isnull"
 
-            default:
-                return nil
+        default:
+            return nil
         }
     }
 }
