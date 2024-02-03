013b1ae052c7aaca457436201ef3aff773ff83d4
Paul Gessinger <hello@paulgessinger.com>
2024-02-03 16:55:28 +0100

    fix: Bug editing matching models

--- swift-paperless/Views/MatchingEditView.swift
+++ swift-paperless/Views/MatchingEditView.swift
@@ -11,38 +11,43 @@ import SwiftUI
 struct MatchEditView<Element>: View where Element: MatchingModel {
     @Binding var element: Element
 
-    @State private var showTextField = false
+//    @State private var showTextField: Bool
+
+//    init(element: Binding<Element>) {
+//        self._element = element
+//        self._showTextField = State(initialValue: element.)
+//    }
+//
+//    static private func
+
+    var showTextField: Bool {
+        switch element.matchingAlgorithm {
+        case .auto, .none:
+            return false
+        default:
+            return true
+        }
+    }
 
     var body: some View {
-        Group {
-            Section {
-                Picker(String(localized: .matching.algorithm), selection: $element.matchingAlgorithm) {
-                    ForEach(MatchingAlgorithm.allCases, id: \.self) { alg in
-                        Text(alg.title).tag(alg)
-                    }
+        Section {
+            Picker(String(localized: .matching.algorithm), selection: $element.matchingAlgorithm) {
+                ForEach(MatchingAlgorithm.allCases, id: \.self) { alg in
+                    Text(alg.title).tag(alg)
                 }
-            } header: {
-                Text(.matching.title)
-            } footer: {
-                Text(element.matchingAlgorithm.label)
-                    .lineLimit(3, reservesSpace: true)
             }
+        } header: {
+            Text(.matching.title)
+        } footer: {
+            Text(element.matchingAlgorithm.label)
+                .lineLimit(3, reservesSpace: true)
+        }
 
+        Section {
             if showTextField {
                 TextField(String(localized: .matching.pattern), text: $element.match)
                     .clearable($element.match)
-
                 Toggle(String(localized: .matching.caseInsensitive), isOn: $element.isInsensitive)
-            }
-        }
-        .onChange(of: element.matchingAlgorithm) { value in
-            withAnimation {
-                switch value {
-                case .auto, .none:
-                    showTextField = false
-                default:
-                    showTextField = true
-                }
             }
         }
     }
