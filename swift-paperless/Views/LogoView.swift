16e27a414948ec3c967792c666edb74ea65a6632
Paul Gessinger <hello@paulgessinger.com>
2024-04-27 09:55:01 +0200

    logo view update

--- swift-paperless/Views/LogoView.swift
+++ swift-paperless/Views/LogoView.swift
@@ -6,19 +6,36 @@
 //
 
 import SwiftUI
+private extension Bundle {
+    var iconFileName: String? {
+        guard let icons = infoDictionary?["CFBundleIcons"] as? [String: Any],
+              let primaryIcon = icons["CFBundlePrimaryIcon"] as? [String: Any],
+              let iconFiles = primaryIcon["CFBundleIconFiles"] as? [String],
+              let iconFileName = iconFiles.last
+        else { return nil }
+        return iconFileName
+    }
+}
 
 struct LogoView: View {
+    private var iconImage: Image {
+        Bundle.main.iconFileName
+            .flatMap { UIImage(named: $0) }
+            .map { Image(uiImage: $0) }!
+    }
+
+    private let logoSize: CGFloat = 64
+    private let logoRadius: CGFloat = 5
+
     var body: some View {
-        HStack {
-            Image(systemName: "leaf.fill")
-                .foregroundColor(.accentColor)
+        VStack {
+            Image("AppLogoOnWhite")
             Text(.localizable.appName)
+                .font(.title)
         }
     }
 }
 
-struct LogoView_Previews: PreviewProvider {
-    static var previews: some View {
-        LogoView()
-    }
+#Preview {
+    LogoView()
 }
