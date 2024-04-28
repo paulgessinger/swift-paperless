f8b413a8d5a2742a4164691a88667e53de58da86
Paul Gessinger <hello@paulgessinger.com>
2024-04-28 13:40:27 +0200

    chore: Add git cliff to fastlane workflow

--- bump.sh
+++ bump.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -u
 
-version=$1
+version=$(git cliff --bumped-version)
+version=$(echo $version | sed 's/v//g')
 
 exec fastlane run increment_version_number_in_xcodeproj version_number:$version
