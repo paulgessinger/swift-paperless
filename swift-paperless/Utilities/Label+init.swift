c0e9dd091ad8e4e639e62fa24e77b9ef5490308e
Paul Gessinger <hello@paulgessinger.com>
2024-05-06 21:33:03 +0200

    Add extension for label from localized string resource

---
+++ swift-paperless/Utilities/Label+init.swift
@@ -0,0 +1,14 @@
+//
+//  Label+init.swift
+//  swift-paperless
+//
+//  Created by Paul Gessinger on 06.05.2024.
+//
+
+import SwiftUI
+
+extension Label where Icon == Image, Title == Text {
+    init(localized: LocalizedStringResource, systemImage: String) {
+        self.init(String(localized: localized), systemImage: systemImage)
+    }
+}
