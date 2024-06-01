87b8cbf2c21ad825705892617055bdcb14a3cf36
Paul Gessinger <hello@paulgessinger.com>
2024-06-01 12:02:40 +0200

    chore: bump to v1.3.0 and update bump script

--- bump.sh
+++ bump.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -u
 
-version=$(git cliff --bumped-version)
-version=$(echo $version | sed 's/v//g')
+version=$1
 
 exec fastlane run increment_version_number_in_xcodeproj version_number:$version
