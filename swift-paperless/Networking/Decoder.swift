55e48a61ba8c4e860b7a34e5ca98cd50002902f1
Paul Gessinger <hello@paulgessinger.com>
2024-06-11 20:15:44 +0200

    fix: Attempt at a fix for date decoding

--- swift-paperless/Networking/Decoder.swift
+++ swift-paperless/Networking/Decoder.swift
@@ -15,24 +15,19 @@ func makeDecoder(tz: TimeZone) -> JSONDecoder {
         let dateStr = try container.decode(String.self)
 
         let iso = ISO8601DateFormatter()
+        iso.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+        if let res = iso.date(from: dateStr) {
+            return res
+        }
+
+        iso.formatOptions = [.withInternetDateTime]
         if let res = iso.date(from: dateStr) {
             return res
         }
 
         let df = DateFormatter()
         df.timeZone = tz
-        df.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSZZZZZ"
-
-        if let res = df.date(from: dateStr) {
-            return res
-        }
-
-        df.dateFormat = "yyyy-MM-ddTHH:mm:ss.SSSSZZZZZ"
-
-        if let res = df.date(from: dateStr) {
-            return res
-        }
-
+        df.locale = Locale(identifier: "en_US_POSIX")
         df.dateFormat = "yyyy-MM-dd"
 
         if let res = df.date(from: dateStr) {
--- swift-paperlessTests/DateDecoderTest.swift
+++ swift-paperlessTests/DateDecoderTest.swift
@@ -10,7 +10,7 @@ import XCTest
 final class DateDecoderTest: XCTestCase {
     func testISO8691() throws {
         let input = "\"2024-05-13T23:38:10.546679Z\"".data(using: .utf8)!
-        let result = try makeDecoder(tz: .current).decode(Date.self, from: input)
+        try makeDecoder(tz: .current).decode(Date.self, from: input)
     }
 
     func testDates() throws {
@@ -21,5 +21,13 @@ final class DateDecoderTest: XCTestCase {
         let input2 = "\"2023-02-18T00:00:00+01:00\"".data(using: .utf8)!
         let date2 = try decoder.decode(Date.self, from: input2)
         XCTAssertNotNil(date2)
+    }
+
+    func testDecodingDocuments() throws {
+        let bundle = Bundle(for: type(of: self))
+        let url = bundle.url(forResource: "issue_91", withExtension: "json")!
+        let data = try Data(contentsOf: url)
+
+        try decoder.decode(ListResponse<Document>.self, from: data)
     }
 }
