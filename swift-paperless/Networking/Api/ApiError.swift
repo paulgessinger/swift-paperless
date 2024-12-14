6715d87b9612ea010e6d0852865ed6c63f077b0c
Paul Gessinger <hello@paulgessinger.com>
2024-12-14 14:50:00 +0100

    docs: Document existing RequestError cases

--- swift-paperless/Networking/Api/ApiError.swift
+++ swift-paperless/Networking/Api/ApiError.swift
@@ -8,11 +8,22 @@
 import Foundation
 
 enum RequestError: Error {
+    // Error building a request in the first place
     case invalidRequest
+
+    // Anything other than HTTPResponse was returned
     case invalidResponse
+
+    // A status code that was not expected was returned
     case unexpectedStatusCode(code: Int)
+
+    // A 403 status code was returned (and was not expected)
     case forbidden(detail: String)
+
+    // A 402 status code was returned (and was not expected)
     case unauthorized(detail: String)
+
+    // A 406 status code was returned. Use by paperless-ngx to indicate that the requested API version is not accepted
     case unsupportedVersion
 }
 
