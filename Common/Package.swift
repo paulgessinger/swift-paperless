511db98d9406fc54c387c2e8b0f4df0175068982
Paul Gessinger <hello@paulgessinger.com>
2025-01-02 12:02:10 +0100

    feat: Add macro boilerplate in Common, add URLMacro as example

--- Common/Package.swift
+++ Common/Package.swift
@@ -1,6 +1,7 @@
 // swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+import CompilerPluginSupport
 import PackageDescription
 
 let package = Package(
@@ -15,9 +16,22 @@ let package = Package(
             targets: ["Common"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/swiftlang/swift-syntax", from: "600.0.1"),
+        .package(url: "https://github.com/pointfreeco/swift-macro-testing", from: "0.5.2"),
+
+    ],
     targets: [
+        .macro(
+            name: "CommonMacros",
+            dependencies: [
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
+            ]
+        ),
         .target(
             name: "Common",
+            dependencies: ["CommonMacros"],
             swiftSettings: [
                 .swiftLanguageMode(.v6),
                 .enableExperimentalFeature("StrictConcurrency"),
@@ -25,7 +39,12 @@ let package = Package(
         ),
         .testTarget(
             name: "CommonTests",
-            dependencies: ["Common"]
+            dependencies: [
+                "Common",
+                "CommonMacros",
+                .product(name: "MacroTesting", package: "swift-macro-testing"),
+                .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax"),
+            ]
         ),
     ]
 )
---
+++ Common/Tests/CommonTests/URLMacroTest.swift
@@ -0,0 +1,95 @@
+//
+//  URLMacroTest.swift
+//  Common
+//
+//  Created by Paul Gessinger on 31.12.24.
+//
+
+import Foundation
+import MacroTesting
+import SwiftSyntax
+import XCTest
+
+@testable import Common
+@testable import CommonMacros
+
+final class URLMacroTest: XCTestCase {
+    override func invokeTest() {
+        withMacroTesting(
+            record: false,
+            macros: ["URL": URLMacro.self]
+        ) {
+            super.invokeTest()
+        }
+    }
+
+    func testValidExpansion() throws {
+        assertMacro { """
+        #URL("https://example.com")
+        """ } expansion: {
+            """
+            URL(string: "https://example.com")!
+            """
+        }
+    }
+
+    func testInvalidURL() throws {
+        assertMacro { """
+        #URL("https://example<>.com")
+        """ } diagnostics: {
+            """
+            #URL("https://example<>.com")
+            ┬────────────────────────────
+            ╰─ 🛑 Malformed URL: "https://example<>.com"
+            """
+        }
+    }
+
+    func testNoLiteral() throws {
+        assertMacro { """
+        #URL(17)
+        """ } diagnostics: {
+            """
+            #URL(17)
+            ┬───────
+            ╰─ 🛑 Not a static string literal
+            """
+        }
+
+        assertMacro { """
+        let url = "https://example.com"
+        #URL(url)
+        """ } diagnostics: {
+            """
+            let url = "https://example.com"
+            #URL(url)
+            ┬────────
+            ╰─ 🛑 Not a static string literal
+            """
+        }
+    }
+
+    func testEmptyArgument() throws {
+        assertMacro { """
+        #URL("")
+        """ } diagnostics: {
+            """
+            #URL("")
+            ┬───────
+            ╰─ 🛑 Malformed URL: ""
+            """
+        }
+    }
+
+    func testNoArgument() throws {
+        assertMacro { """
+        #URL()
+        """ } diagnostics: {
+            """
+            #URL()
+            ┬─────
+            ╰─ 🛑 Not a static string literal
+            """
+        }
+    }
+}
