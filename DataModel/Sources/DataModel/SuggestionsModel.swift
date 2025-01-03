4be7b7af3ef0dcf5707c091201f0686f7e25eca5
Paul Gessinger <hello@paulgessinger.com>
2025-01-03 17:42:44 +0100

    refactor: Migrate Suggestions and Tags to MetaCodable

--- DataModel/Sources/DataModel/SuggestionsModel.swift
+++ DataModel/Sources/DataModel/SuggestionsModel.swift
@@ -6,26 +6,15 @@
 //
 
 import Foundation
+import MetaCodable
 
-public struct Suggestions: Codable, Sendable {
+@Codable
+@CodingKeys(.snake_case)
+@MemberInit
+public struct Suggestions: Sendable {
     public var correspondents: [UInt]
     public var tags: [UInt]
     public var documentTypes: [UInt]
     public var storagePaths: [UInt]
     public var dates: [Date]
-
-    public init(correspondents: [UInt] = [], tags: [UInt] = [], documentTypes: [UInt] = [], storagePaths: [UInt] = [], dates: [Date] = []) {
-        self.correspondents = correspondents
-        self.tags = tags
-        self.documentTypes = documentTypes
-        self.storagePaths = storagePaths
-        self.dates = dates
-    }
-
-    private enum CodingKeys: String, CodingKey {
-        case correspondents, tags
-        case documentTypes = "document_types"
-        case storagePaths = "storage_paths"
-        case dates
-    }
 }
