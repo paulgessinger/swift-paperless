4192fada507157fa4624d6ce6ccdf6b3fc70cf32
Paul Gessinger <hello@paulgessinger.com>
2025-01-03 17:12:05 +0100

    refactor: Switch first tranche of models over to MetaCodable

--- DataModel/Sources/DataModel/FilterRule.swift
+++ DataModel/Sources/DataModel/FilterRule.swift
@@ -11,7 +11,7 @@ import os
 
 extension FilterRuleType: Codable {}
 
-public enum FilterRuleValue: Codable, Equatable, Sendable {
+public enum FilterRuleValue: Equatable, Sendable {
     case date(value: Date)
     case number(value: Int)
     case tag(id: UInt)
@@ -155,11 +155,6 @@ public struct FilterRule: Equatable, Sendable {
         }
     }
 
-    private enum CodingKeys: String, CodingKey {
-        case ruleType = "rule_type"
-        case value
-    }
-
     public static func queryItems(for rules: [FilterRule]) -> [URLQueryItem] {
         var result: [URLQueryItem] = []
 
@@ -200,6 +195,11 @@ public struct FilterRule: Equatable, Sendable {
 }
 
 extension FilterRule: Codable {
+    private enum CodingKeys: String, CodingKey {
+        case ruleType = "rule_type"
+        case value
+    }
+
     public init(from decoder: any Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         ruleType = try container.decode(FilterRuleType.self, forKey: .ruleType)
--- DataModel/Sources/DataModel/Metadata.swift
+++ DataModel/Sources/DataModel/Metadata.swift
@@ -6,15 +6,19 @@
 //
 
 import Foundation
+import MetaCodable
 
-public struct Metadata: Decodable, Sendable {
+@Codable
+@CodingKeys(.snake_case)
+@MemberInit
+public struct Metadata: Sendable {
     public var originalChecksum: String
     public var originalSize: Int64
     public var originalMimeType: String
     public var mediaFilename: String
     public var hasArchiveVersion: Bool
 
-    public struct Item: Decodable, Sendable {
+    public struct Item: Codable, Sendable {
         public var namespace: String
         public var prefix: String
         public var key: String
@@ -31,34 +35,4 @@ public struct Metadata: Decodable, Sendable {
     public var archiveMetadata: [Item]?
 
     public var lang: String
-
-    public init(originalChecksum: String, originalSize: Int64, originalMimeType: String, mediaFilename: String, hasArchiveVersion: Bool, originalMetadata: [Item], archiveChecksum: String? = nil, archiveMediaFilename: String? = nil, originalFilename: String, archiveSize: Int64? = nil, archiveMetadata: [Item]? = nil, lang: String) {
-        self.originalChecksum = originalChecksum
-        self.originalSize = originalSize
-        self.originalMimeType = originalMimeType
-        self.mediaFilename = mediaFilename
-        self.hasArchiveVersion = hasArchiveVersion
-        self.originalMetadata = originalMetadata
-        self.archiveChecksum = archiveChecksum
-        self.archiveMediaFilename = archiveMediaFilename
-        self.originalFilename = originalFilename
-        self.archiveSize = archiveSize
-        self.archiveMetadata = archiveMetadata
-        self.lang = lang
-    }
-
-    private enum CodingKeys: String, CodingKey {
-        case originalChecksum = "original_checksum"
-        case originalSize = "original_size"
-        case originalMimeType = "original_mime_type"
-        case mediaFilename = "media_filename"
-        case hasArchiveVersion = "has_archive_version"
-        case originalMetadata = "original_metadata"
-        case archiveChecksum = "archive_checksum"
-        case archiveSize = "archive_size"
-        case archiveMediaFilename = "archive_media_filename"
-        case originalFilename = "original_filename"
-        case archiveMetadata = "archive_metadata"
-        case lang
-    }
 }
