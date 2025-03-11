26141d5ce4f043dd1f04478ce80a652ad7617cd3
Paul Gessinger <hello@paulgessinger.com>
2025-03-11 22:42:18 +0100

    feat: Add calculation of change perms

--- DataModel/Sources/DataModel/UserModel.swift
+++ DataModel/Sources/DataModel/UserModel.swift
@@ -16,6 +16,38 @@ public struct User: Model, Identifiable, Equatable, Sendable {
     @CodedAt("is_superuser")
     public var isSuperUser: Bool
     public var username: String
+
+    @Default([UInt]())
+    public var groups: [UInt]
+}
+
+public extension User {
+    func canChange(_ resource: some PermissionsModel) -> Bool {
+        if isSuperUser {
+            return true
+        }
+
+        // If resource has no owner, it's accessible to all
+        if resource.owner == nil || resource.owner == id {
+            return true
+        }
+
+        guard let permissions = resource.permissions else {
+            return false
+        }
+
+        if permissions.change.users.contains(id) {
+            return true
+        }
+
+        for group in groups {
+            if permissions.change.groups.contains(group) {
+                return true
+            }
+        }
+
+        return false
+    }
 }
 
 @Codable
