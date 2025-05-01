17bb1e301ef9c0ea66f924187660d50b55baefcb
Paul Gessinger <hello@paulgessinger.com>
2025-05-01 16:29:33 +0100

    feat: Add slugify function to Common

---
+++ Common/Tests/CommonTests/Slugify.swift
@@ -0,0 +1,33 @@
+//
+//  Slugify.swift
+//  swift-paperlessTests
+//
+//  Created by Paul Gessinger on 01.05.25
+//
+
+@testable import Common
+import Foundation
+import SwiftUI
+import Testing
+
+@Test func testSlugify() throws {
+    #expect("Some/Name".slugify() == "Some-Name")
+    #expect("SomeüName".slugify() == "SomeuName")
+    #expect("Some Name".slugify() == "Some Name")
+    #expect("Some;Name".slugify() == "Some-Name")
+    #expect("Hyvee 2025-03-30".slugify() == "Hyvee 2025-03-30")
+}
+
+@Test func testPathAssembly() throws {
+    let file = URL("file:///some/path/to/a/file/This Has Some not ök ø chars.pdf")!
+
+    let ext = file.pathExtension
+    let stem = file.deletingPathExtension().lastPathComponent
+    #expect(stem == "This Has Some not ök ø chars")
+
+    let slug = stem.slugify()
+    #expect(slug == "This Has Some not ok - chars")
+
+    let filename = ext.isEmpty ? stem : "\(slug).\(ext)"
+    #expect(filename == "This Has Some not ok - chars.pdf")
+}
