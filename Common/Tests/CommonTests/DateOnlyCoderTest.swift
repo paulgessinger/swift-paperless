ab96b088d48f462952883852c9a64738ff5a417c
Paul Gessinger <hello@paulgessinger.com>
2025-05-25 22:15:25 +0200

    fix: DateOnlyCoder uses regex to strip time + timezone
    For DateOnlyCoding, we assume the device local timezone when parsing. For this, we now strip out everything except for the date from the input string, and only then run the formatter. This produces a naive Date object in the local timezone. When we send it back to the backend, we only format the date component, so roundtripping should be safe in all cases. 🤞

--- Common/Tests/CommonTests/DateOnlyCoderTest.swift
+++ Common/Tests/CommonTests/DateOnlyCoderTest.swift
@@ -36,23 +36,41 @@ func testDateOnlyCoderDecode() throws {
     let decoder = JSONDecoder()
 
     var decoded = try decoder.decode(TestStruct.self, from: dateOnly)
+    print(decoded)
 
     let exp = try #require(Calendar.current.date(from: DateComponents(year: 2023, month: 12, day: 4, hour: 0, minute: 0, second: 0)))
     #expect(decoded.created == exp)
 
-    let tz = TimeZone(secondsFromGMT: 60 * 60)!
     let dateTime = try #require(#"{"created":"2023-12-04T09:10:24+01:00"}"#.data(using: .utf8))
 
     decoded = try decoder.decode(TestStruct.self, from: dateTime)
 
-    var cal = Calendar.current
-    cal.timeZone = tz
+    let cal = Calendar.current
     let components = cal.dateComponents([.year, .month, .day, .hour, .minute, .second], from: decoded.created)
 
     #expect(components.year == 2023)
     #expect(components.month == 12)
     #expect(components.day == 4)
-    #expect(components.hour == 9)
-    #expect(components.minute == 10)
-    #expect(components.second == 24)
+    #expect(components.hour == 0)
+    #expect(components.minute == 0)
+    #expect(components.second == 0)
+}
+
+@Test("Arbitrary timezones get stripped out", arguments: 1 ... 10)
+func testArbitraryTimezonesGetStrippedOut(offset: Int) throws {
+    let dateOnly = try #require("{\"created\":\"2028-11-02T00:00:00+0\(offset):00\"}".data(using: .utf8))
+
+    let decoder = JSONDecoder()
+
+    let decoded = try decoder.decode(TestStruct.self, from: dateOnly)
+
+    let cal = Calendar.current
+    let components = cal.dateComponents([.year, .month, .day, .hour, .minute, .second], from: decoded.created)
+
+    #expect(components.year == 2028)
+    #expect(components.month == 11)
+    #expect(components.day == 2)
+    #expect(components.hour == 0)
+    #expect(components.minute == 0)
+    #expect(components.second == 0)
 }
