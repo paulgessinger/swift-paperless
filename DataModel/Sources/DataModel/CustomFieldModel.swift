babfb42ecaf0be67fd34bb66d88457db43209fa7
Paul Gessinger <hello@paulgessinger.com>
2025-06-08 20:55:11 +0200

    feat: Add data model for custom fields entities

---
+++ DataModel/Sources/DataModel/CustomFieldModel.swift
@@ -0,0 +1,96 @@
+//
+//  CustomFieldModel.swift
+//  DataModel
+//
+//  Created by AI Assistant on 26.03.2024.
+//
+
+import Common
+import Foundation
+import MetaCodable
+
+public enum CustomFieldDataType: RawRepresentable, Codable, Equatable, Hashable, Sendable {
+    case string
+    case url
+    case date
+    case boolean
+    case integer
+    case float
+    case monetary
+    case documentlink
+    case select
+    case other(String)
+
+    public init?(rawValue: String) {
+        switch rawValue {
+        case "string": self = .string
+        case "url": self = .url
+        case "date": self = .date
+        case "boolean": self = .boolean
+        case "integer": self = .integer
+        case "float": self = .float
+        case "monetary": self = .monetary
+        case "documentlink": self = .documentlink
+        case "select": self = .select
+        default: self = .other(rawValue)
+        }
+    }
+
+    public var rawValue: String {
+        switch self {
+        case .string: "string"
+        case .url: "url"
+        case .date: "date"
+        case .boolean: "boolean"
+        case .integer: "integer"
+        case .float: "float"
+        case .monetary: "monetary"
+        case .documentlink: "documentlink"
+        case .select: "select"
+        case let .other(value): value
+        }
+    }
+}
+
+@Codable
+@CodingKeys(.snake_case)
+@MemberInit
+public struct CustomFieldSelectOption: Identifiable, Hashable, Sendable {
+    public var id: String
+    public var label: String
+}
+
+@Codable
+@CodingKeys(.snake_case)
+@MemberInit
+public struct CustomFieldExtraData: Sendable, Hashable {
+    @Default([CustomFieldSelectOption]())
+    public var selectOptions: [CustomFieldSelectOption]
+
+    @Default(nil as String?)
+    @CodedBy(NullCoder<String>())
+    public var defaultCurrency: String?
+
+    @usableFromInline
+    static var `default`: Self { .init() }
+}
+
+@Codable
+@CodingKeys(.snake_case)
+@MemberInit
+public struct CustomField: Identifiable, Model, Hashable, Named, Sendable {
+    public typealias SelectOption = CustomFieldSelectOption
+    public typealias ExtraData = CustomFieldExtraData
+    public typealias DataType = CustomFieldDataType
+
+    public var id: UInt
+    public var name: String
+    public var dataType: DataType
+
+    @Default(CustomFieldExtraData.default)
+    public var extraData: ExtraData
+
+    @IgnoreEncoding
+    @Default(nil as UInt?)
+    public var documentCount: UInt?
+}
