db0373b52f85969df02e57fad4f49c75bc11905e
Paul Gessinger <hello@paulgessinger.com>
2026-01-01 15:17:54 +0100

    fix: Improve handling of unsupported custom field types

--- swift-paperless/Views/CustomFields/CustomFieldEditView.swift
+++ swift-paperless/Views/CustomFields/CustomFieldEditView.swift
@@ -168,10 +168,10 @@ struct CustomFieldsEditView: View {
 
   @ViewBuilder
   private func fieldView(index: Int, field: Binding<CustomFieldInstance>) throws -> some View {
-    if case .invalid(let reason) = field.wrappedValue.value {
-      InvalidFieldView(instance: field.wrappedValue, reason: reason)
-    } else {
-      Group {
+    Group {
+      if case .invalid(let reason) = field.wrappedValue.value {
+        InvalidFieldView(instance: field.wrappedValue, reason: reason)
+      } else {
         switch field.wrappedValue.field.dataType {
         case .string:
           StringView(instance: field)
@@ -198,12 +198,12 @@ struct CustomFieldsEditView: View {
           InvalidFieldView(instance: field.wrappedValue, reason: .unknownDataType(dataType))
         }
       }
-      .swipeActions {
-        Button(.customFields(.delete), role: .destructive) {
-          deleteField(index: index)
-        }
-        .disabled(!isEnabled)
+    }
+    .swipeActions {
+      Button(.customFields(.delete), role: .destructive) {
+        deleteField(index: index)
       }
+      .disabled(!isEnabled)
     }
   }
 
@@ -303,6 +303,7 @@ struct CustomFieldsEditView: View {
 
     .onChange(of: customFields) {
       document.customFields = customFields.rawEntries
+      showInvalidFields = customFields.hasInvalidValues
     }
   }
 }
