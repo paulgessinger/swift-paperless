abb8f6aa2a8f5b456afa63b7df9e1726e63f8a91
Paul Gessinger <hello@paulgessinger.com>
2026-01-10 14:59:21 +0100

    scaffold minimal app for flow testing

---
+++ OAuthTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OAuthTest
+//
+//  Created by Paul Gessinger on 10.01.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
