719d5965adef352758284cb91164b1fb49607eac
Paul Gessinger <hello@paulgessinger.com>
2026-03-29 11:08:53 +0200

    refactor: Another Swift 6.3 concurrency fix
    Interact with NSItemProvider on the main thread but do processing in the background

--- ShareExtension/AttachmentManager+receiveAttachment.swift
+++ ShareExtension/AttachmentManager+receiveAttachment.swift
@@ -11,21 +11,46 @@ import os
 private struct InvalidAttachmentContent: Error {}
 
 extension AttachmentManager {
-  private nonisolated
-    static func loadItem(attachment: NSItemProvider, type: String) async throws -> URL
+  private enum LoadedItem: Sendable {
+    case url(URL)
+    case data(Data)
+    case image(UIImage)
+  }
+
+  @MainActor
+  private static func loadItemFromProvider(attachment: NSItemProvider, type: String) async throws
+    -> LoadedItem
   {
     let data = try await attachment.loadItem(forTypeIdentifier: type, options: nil)
-
     Logger.shared.info("Loading item for type \(type, privacy: .public)")
 
     switch data {
     case let url as URL:
       Logger.shared.info("Received url \(url, privacy: .public)")
-      return url
+      return .url(url)
 
     case let data as NSData:
-      Logger.shared.info("Received data length \(data.count) bytes, writing to temporary file")
-      let mime = data.mimeType
+      Logger.shared.info("Received data length \(data.count) bytes")
+      return .data(data as Data)
+
+    case let image as UIImage:
+      Logger.shared.info("Received UIImage")
+      return .image(image)
+
+    default:
+      Logger.shared.error(
+        "Got attachment data \(String(describing: data), privacy: .public) but cannot handle")
+      throw InvalidAttachmentContent()
+    }
+  }
+
+  private nonisolated static func processLoadedItem(_ item: LoadedItem) throws -> URL {
+    switch item {
+    case .url(let url):
+      return url
+
+    case .data(let data):
+      let mime = (data as NSData).mimeType
       Logger.shared.info("Got mime type \(mime, privacy: .public)")
 
       let ext: String
@@ -42,45 +67,33 @@ extension AttachmentManager {
         Logger.shared.error("Got mime type \(mime, privacy: .public) but cannot handle")
         throw InvalidAttachmentContent()
       }
+
       Logger.shared.info(
         "Mapped mime \(mime, privacy: .public) to extension \(ext, privacy: .public)")
-
       let url = FileManager.default.temporaryDirectory
         .appending(component: formattedImportFilename(prefix: "Import"))
         .appendingPathExtension(ext)
+      try data.write(to: url)
+      return url
 
-      do {
-        try data.write(to: url)
-        return url
-      } catch {
-        Logger.shared.error("Unable to store data to temporary file: \(error, privacy: .public)")
-        throw error
-      }
-
-    case let image as UIImage:
-      Logger.shared.info("Received UIImage, saving as PNG")
-
-      if let data = image.pngData() {
-        let url = FileManager.default.temporaryDirectory
-          .appending(component: formattedImportFilename(prefix: "Import"))
-          .appendingPathExtension("png")
-        do {
-          try data.write(to: url)
-          return url
-        } catch {
-          Logger.shared.error("Unable to store data to temporary file: \(error, privacy: .public)")
-          throw error
-        }
-      } else {
+    case .image(let image):
+      let url = FileManager.default.temporaryDirectory
+        .appending(component: formattedImportFilename(prefix: "Import"))
+        .appendingPathExtension("png")
+      guard let data = image.pngData() else {
         Logger.shared.error("Unable to convert image to PNG")
         throw InvalidAttachmentContent()
       }
-
-    default:
-      Logger.shared.error(
-        "Got attachment data \(String(describing: data), privacy: .public) but cannot handle")
-      throw InvalidAttachmentContent()
+      try data.write(to: url)
+      return url
     }
+  }
+
+  private static func loadItem(attachment: NSItemProvider, type: String) async throws -> URL {
+    let item = try await loadItemFromProvider(attachment: attachment, type: type)
+    return try await Task.detached {
+      try processLoadedItem(item)
+    }.value
   }
 
   func receive(attachments: sending [NSItemProvider]) {
