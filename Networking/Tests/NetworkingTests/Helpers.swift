90247c90e49d3604a9c470d4245d4b8883003dad
Paul Gessinger <hello@paulgessinger.com>
2026-04-18 09:37:49 +0200

    lint, test fix, reduce metacodable

---
+++ Networking/Tests/NetworkingTests/Helpers.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+func testData(_ file: String) -> Data? {
+  guard let rel = URL(string: file) else {
+    return nil
+  }
+  guard
+    let url = Bundle.module.url(
+      forResource: rel.deletingPathExtension().absoluteString,
+      withExtension: ".\(rel.pathExtension)")
+  else {
+    return nil
+  }
+
+  do {
+    return try Data(contentsOf: url)
+  } catch {
+    return nil
+  }
+}
