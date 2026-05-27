97b8c9d4cef51de6ea4473c6152d7cf548ae3ca0
Paul Gessinger <hello@paulgessinger.com>
2026-05-27 22:17:18 +0300

    fix: Missing network monitor in preview

--- swift-paperless/Views/Document/DocumentView.swift
+++ swift-paperless/Views/Document/DocumentView.swift
@@ -767,6 +767,7 @@ struct DocumentView: View {
   @Previewable @StateObject var store = DocumentStore(repository: PreviewRepository())
   @Previewable @StateObject var errorController = ErrorController()
   @Previewable @StateObject var connectionManager = ConnectionManager()
+  @Previewable @State var networkMonitor = NetworkMonitor()
   @Previewable @State var showSettings = false
 
   DocumentView(showSettings: $showSettings)
@@ -774,4 +775,5 @@ struct DocumentView: View {
     .environmentObject(errorController)
     .environmentObject(connectionManager)
     .environment(RouteManager())
+    .environment(networkMonitor)
 }
